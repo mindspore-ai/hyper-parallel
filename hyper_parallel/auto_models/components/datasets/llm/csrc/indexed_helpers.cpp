23dd3d0edaa6fc8c1d7b406d068fd974ac36dda2
townwish <townwish2023@outlook.com>
2026-08-25 15:54:49 +0800

    fix(auto-models): correct C++ include order

--- hyper_parallel/auto_models/components/datasets/llm/csrc/indexed_helpers.cpp
+++ hyper_parallel/auto_models/components/datasets/llm/csrc/indexed_helpers.cpp
@@ -13,12 +13,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
 #include <algorithm>
 #include <cstdint>
 #include <vector>
-
-#include <pybind11/numpy.h>
-#include <pybind11/pybind11.h>
 
 namespace py = pybind11;
 
