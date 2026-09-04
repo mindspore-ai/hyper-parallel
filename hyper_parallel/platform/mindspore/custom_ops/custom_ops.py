26e1d8b7724414c9650681d998dc1ac38b7f6f21
wang_hua_2025 <wanghua36@huawei.com>
2026-09-04 14:52:49 +0800

    feat(custom-ops): add mhc_pre_cmhc, mhc_head and situ_glu operators
    Squash of 3 PRs from Somnus2020/hyper-parallel r1.0.0:
    
    - !1 add npu_mhc_pre_cmhc operator (ascend kernels, DFunction, public api, parallel op)
    
    - !2 integrate npu_mhc_pre_cmhc into framework and align UT with 8-arg forward contract
    
    - !3 add mhc_head and npu_situ_glu fused operators

--- hyper_parallel/platform/mindspore/custom_ops/custom_ops.py
+++ hyper_parallel/platform/mindspore/custom_ops/custom_ops.py
@@ -24,7 +24,10 @@ from hyper_parallel.platform.mindspore.custom_ops.custom_op_impl import (
     NpuLightningIndexerDFunction,
     NpuMhcPostDFunction,
     NpuMhcPreClampSinkhornDFunction,
+    NpuMhcPreCmhcDFunction,
+    NpuMhcHeadDFunction,
     NpuMhcPreSinkhornDFunction,
+    NpuSituGluDFunction,
     NpuSparseFlashMlaDFunction,
     NpuSparseLightningIndexerGradKlLossDFunction,
     NpuSparseLightningIndexerKlLossGradDFunction,
@@ -63,6 +66,21 @@ class MindSporeCustomOps:
     @staticmethod
     def npu_mhc_pre_clamp_sinkhorn(*args, **kwargs):
         return NpuMhcPreClampSinkhornDFunction.apply(*args, **kwargs)
+
+    @staticmethod
+    def npu_mhc_pre_cmhc(*args, **kwargs):
+        """Apply the NPU MHC pre-CMHC fused custom operator."""
+        return NpuMhcPreCmhcDFunction.apply(*args, **kwargs)
+
+    @staticmethod
+    def npu_mhc_head(*args, **kwargs):
+        """Apply the NPU MHC head (residual-stream collapse) custom operator."""
+        return NpuMhcHeadDFunction.apply(*args, **kwargs)
+
+    @staticmethod
+    def npu_situ_glu(*args, **kwargs):
+        """Apply the NPU SiTU-GLU fused activation custom operator."""
+        return NpuSituGluDFunction.apply(*args, **kwargs)
 
     @staticmethod
     def npu_lightning_indexer(*args, **kwargs):
